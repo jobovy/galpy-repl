cbce7ddae9df5279de7bf4a44ab17ed2024f1c31
Jo Bovy <jo.bovy@gmail.com>
2022-02-25 12:46:51 -0500

    Make sure to use astropy to parse integration times with units

--- prerun.py
+++ prerun.py
@@ -12,4 +12,8 @@ import astropy.units as u
 # Set up galpy to return outputs as astropy Quantities
 import galpy.util.conversion
 galpy.util.conversion._APY_UNITS=True
+# Also need to set the following, because pyodide SkyCoord failure prevents this from being set correctly in Orbits
+import galpy.orbit.Orbits
+galpy.orbit.Orbits._APY_LOADED= True
+# Inline plots
 %matplotlib inline
