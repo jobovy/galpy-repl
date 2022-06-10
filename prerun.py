4d79194c9ee5ecb72186c8329c5f45e200e9a27f
Jo Bovy <bovy@astro.utoronto.ca>
2022-06-10 16:10:03 -0400

    Revert "Switch back to pyodide dev"
    This reverts commit 415957baf00c9b8bcc09a27b4a320768c6cb1e5f.

--- prerun.py
+++ prerun.py
@@ -2,9 +2,8 @@
 # Install astroquery
 import micropip
 await micropip.install('astroquery')
-# Install galpy and its dependencies
-import pyodide_js
-await pyodide_js.loadPackage(["numpy", "scipy", "matplotlib", "astropy", "future", "setuptools","https://www.galpy.org/wheelhouse/galpy-latest-cp310-cp310-emscripten_3_1_6_wasm32.whl"])
+# Install galpy
+await micropip.install('https://www.galpy.org/wheelhouse/galpy-latest-py3-none-any.whl')
 # Turn off warnings
 import warnings
 from galpy.util import galpyWarning
